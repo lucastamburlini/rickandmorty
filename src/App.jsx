4977b28f2d1562d03e031aef9388ba5d23e5032e
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-08-22 16:38:44 -0300

    Changes in the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Cards from './components/Cards.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import characters from './data.js';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   function searchHandler() {
+      window.alert("El ID que voy a buscar")
+   }
+
+   function closeHandler() {
+      window.alert('Emulamos que se cierra la card')
+   }
+
+   return (
+      <div className='App'>
+         <SearchBar onSearch={searchHandler} />
+         <Cards characters={characters} onClose={closeHandler} />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+export default App;
