3f032ccbbe58b45816c01876c42c9c6b30543deb
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-08 16:43:34 -0300

    hw-05

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,6 +1,14 @@
+const users = require("../utils/users.js")
+
 const login = (req, res) => {
     const { email, password } = req.query
-    console.log(req);
+
+    const userFound = users.find((user) => user.email === email && user.password === password)
+    userFound ?
+        res.status(200).json({ access: true })
+        :
+        res.status(404).json({ access: false })
+
 }
 
 module.exports = {
