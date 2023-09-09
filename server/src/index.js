63ab1bdcb321a2708a5bbc56d4160330bea70a76
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-09 10:07:58 -0300

    hw-05

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const server = express();
 const PORT = 3001;
+const mainRouter = require("./routes/index")
+const morgan = require("morgan")
+
+server.use(express.json());
+server.use(morgan("dev"))
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+server.use("/rickandmorty", mainRouter)
+
+
 
 server.listen(PORT, () => {
     console.log('Server raised in port: ' + PORT);
+
 });
+
+
 
 
 
