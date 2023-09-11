ae0eb193ebfa1a135b7e1adc85a56bc73ddb28e5
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-11 13:35:06 -0300

    hw-06

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -31,6 +31,32 @@ const getCharById = async (req, res) => {
 
 }
 
+/* const getCharById = (req, res) => {
+    const { id } = req.params
+    axios(`${URL}/${id}`)
+        .then((response) => response.data)
+        .then(({ id, status, name, species, origin, image, gender }) => {
+            if (name) {
+                const character = {
+                    id,
+                    status,
+                    name,
+                    species,
+                    origin,
+                    image,
+                    gender
+                }
+                res.status(200).json(character)
+            } else {
+                res.status(404).send('Character not found');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send(error.message);
+        });
+} */
+
 
 module.exports = {
     getCharById
