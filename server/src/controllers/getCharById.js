c04d8369d55e7c288c4dec954d83c4f4b6a8df8c
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-11 13:34:21 -0300

    hw-06

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -1,30 +1,34 @@
 const URL = "https://rickandmortyapi.com/api/character"
 const axios = require("axios")
 
-const getCharById = (req, res) => {
-    const { id } = req.params
-    axios(`${URL}/${id}`)
-        .then((response) => response.data)
-        .then(({ id, status, name, species, origin, image, gender }) => {
-            if (name) {
-                const character = {
-                    id,
-                    status,
-                    name,
-                    species,
-                    origin,
-                    image,
-                    gender
-                }
-                res.status(200).json(character)
-            } else {
-                res.status(404).send('Character not found');
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send(error.message);
-        });
+const getCharById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios(`${URL}/${id}`);
+        const { data } = response;
+
+        if (data) {
+            const { id, status, name, species, origin, image, gender } = data;
+            const character = {
+                id,
+                status,
+                name,
+                species,
+                origin,
+                image,
+                gender
+            };
+            res.status(200).json(character)
+        } else {
+            res.status(404).send('Character not found');
+        }
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).send({ message: error.message });
+
+    }
+
 }
 
 
