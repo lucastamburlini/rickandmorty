854f4a63691c558f20dfd2308cd76421d2fc0060
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-12 16:58:07 -0300

    hw-07

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -2,16 +2,38 @@ const app = require("../src/app");
 const session = require("supertest");
 const agent = session(app)
 
-describe("Test de rutas", () => {
+describe("Route test", () => {
 
     describe('GET /rickandmorty/character/:id', () => {
-        it('Responde con status: 200', async () => {
+        it('Responds with status: 200', async () => {
             await agent.get('/rickandmorty/character/1').expect(200);
         })
 
         it('Responds to an object with the properties: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
+            const response = await agent.get('/rickandmorty/character/1');
+            expect(response.body).toHaveProperty("id");
+            expect(response.body).toHaveProperty("name");
+            expect(response.body).toHaveProperty("species");
+            expect(response.body).toHaveProperty("gender");
+            expect(response.body).toHaveProperty("status");
+            expect(response.body).toHaveProperty("origin");
+            expect(response.body).toHaveProperty("image");
+        });
 
+        it('If there is an error, respond with status: 500', async () => {
+            await agent.get('/rickandmorty/character/none').expect(500);
         })
     })
 
+})
+
+describe("GET /rickandmorty/login", () => {
+    it("Correct information: most return: { access: true }", async () => {
+        const { body } = await agent.get("/rickandmorty/login/?email=ejemplo@gmail.com&password=hola1234*")
+        expect(body.access).toEqual(true)
+    })
+    it("Incorrect information: most return: { access: false }", async () => {
+        const { body } = await agent.get("/rickandmorty/login/?email=correoincorrecto@gmail.com&password=contraseniaincorrecta")
+        expect(body.access).toEqual(false)
+    })
 })
