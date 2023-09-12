d0b5363f07ae3794931166cab216113acb05363b
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-12 15:54:18 -0300

    hw-07

---
+++ server/test/index.test.js
@@ -0,0 +1,13 @@
+const app = require("../src/app");
+const session = require("supertest");
+const agent = session(app)
+
+describe('GET /rickandmorty/character/:id', () => {
+    it('Responde con status: 200', async () => {
+        await agent.get('/rickandmorty/character/1').expect(200);
+    })
+
+    it('Responds to an object with the properties: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
+
+    })
+})
