6ae23fdb3a230381b29f5d93abb8c05f9391601f
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-20 18:46:40 -0300

    hw-03

--- server/src/models/Favorite.js
+++ server/src/models/Favorite.js
@@ -1,5 +1,28 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('Favorite', {}, { timestamps: false });
+   sequelize.define('Favorite', {
+      id: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+      },
+      name: {
+         type: DataTypes.STRING,
+      },
+      status: {
+         type: DataTypes.STRING,
+      },
+      species: {
+         type: DataTypes.STRING,
+      },
+      gender: {
+         type: DataTypes.STRING,
+      },
+      origin: {
+         type: DataTypes.STRING,
+      },
+      image: {
+         type: DataTypes.STRING,
+      },
+   }, { timestamps: false });
 };
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,19 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('User', {}, { timestamps: false });
+   sequelize.define('User', {
+      id: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+      },
+      email: {
+         type: DataTypes.STRING,
+         isEmail: true,
+         allowNull: false,
+         unique: true
+      },
+      password: {
+         type: DataTypes.STRING,
+      }
+   }, { timestamps: false });
 };
