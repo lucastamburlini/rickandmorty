a13c236fc8434a9161de6ca86d3b038611e3f88f
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-21 19:39:20 -0300

    hw-04

--- server/src/controllers/postUser.js
+++ server/src/controllers/postUser.js
@@ -1,9 +1,9 @@
-const User = require("../DB_connection")
+const { User } = require("../DB_connection")
 
 const postUser = async (req, res) => {
     try {
         const { email, password } = req.body;
-        if (!email || !password) return res.status(400).send("Faltan datos")
+        if (!email || !password) return res.status(400).send("Missing data")
         const response = await User.findOrCreate({ where: { email: email, password: password } })
         return res.json(response)
     } catch (error) {
