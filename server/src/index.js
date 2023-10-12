e48adeec7b1970c8772aa6faca715aefb15d2eac
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-12 19:00:46 -0300

    [add] deploy

--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app")
-const { conn } = require("./DB_connection")
-const PORT = 3001;
-
-app.listen(PORT, () => {
-    conn.sync({ force: false })
-    console.log('app raised in port: ' + PORT);
-});
